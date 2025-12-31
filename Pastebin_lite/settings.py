b344f7c9ca9fca7df07fa3852dc0a0f075d989a7
sangamithra-18 <sangami.rengaraj1895@gmail.com>
2025-12-31 17:06:54 +0530

    Fix database config for Render

--- Pastebin_lite/settings.py
+++ Pastebin_lite/settings.py
@@ -118,10 +118,11 @@ WSGI_APPLICATION = 'Pastebin_lite.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
